c65a8229461b9b493c907125a69d5784f2efc517
willigerber <willi.gerber@gmail.com>
2023-06-03 00:11:20 -0300

    nada

--- src/app/modules/question/question.component.ts
+++ src/app/modules/question/question.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent {
-  public typeForm: boolean = true;
+  public typeForm: boolean = false;
 
 }
